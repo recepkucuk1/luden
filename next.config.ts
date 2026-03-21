f26471c0078a07820a0b9611712668af0e3586c2
Recep Küçük <recepkucuk@Receps-MacBook-Air.local>
2026-03-21 20:50:56 +0300

    CSP: hCaptcha domain'leri eklendi

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,12 @@ const nextConfig: NextConfig = {
           value: [
             "default-src 'self'",
             // unsafe-eval: @react-pdf/renderer WASM/fontkit için gerekli
-            "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.hcaptcha.com",
             "style-src 'self' 'unsafe-inline'",
             "img-src 'self' data: blob:",
             "font-src 'self'",
-            "connect-src 'self'",
+            "frame-src 'self' https://newassets.hcaptcha.com",
+            "connect-src 'self' https://hcaptcha.com https://sentry.hcaptcha.com",
             "worker-src 'self' blob:",
           ].join("; "),
         },
