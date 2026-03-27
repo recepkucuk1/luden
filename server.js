c988cb01f8225710977979a1adcf4e2055752ca8
Recep Küçük <recepkucuk@Receps-MacBook-Air.local>
2026-03-27 12:18:49 +0300

    fix: custom server.js eklendi - Hostinger MIME type düzeltmesi

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
-
-const app = next({ dev: false })
-const handle = app.getRequestHandler()
+const next = require('next');
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3000, () => {
-    console.log('Server running on port 3000')
-  })
-})
+  require('http').createServer((req, res) => handle(req, res))
+    .listen(process.env.PORT || 3000, () => {
+      console.log('Server ready on port', process.env.PORT || 3000);
+    });
+});
